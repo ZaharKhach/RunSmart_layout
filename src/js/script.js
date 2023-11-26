298650800290d5a2c093967088f5e326b3edbb91
dendywool <zaharkhachaturyan@gmail.com>
2023-11-26 15:48:46 +0100

    silders with mobile adaptation (without veery small screen)

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
     $('.carousel__inner').slick({
         speed: 900,
@@ -16,6 +17,19 @@ $(document).ready(function () {
 
     const catalogContent = document.querySelector('.catalog__content');
     const catalogWrapper = document.querySelector('.catalog__wrapper');
+    const filters = document.querySelectorAll('.catalog__tab');
+    
+
+    filters.forEach(filter => {
+        filter.addEventListener('click', (e) => {
+            for(let i = 0; i < filters.length; i++) {
+                filters[i].classList.remove('catalog__tab_active');
+                if(filters[i] == e.target.parentNode) {
+                    filters[i].classList.add('catalog__tab_active')
+                }
+            }
+        })
+    })
 
     const linksListener = (catalogContent) => {
         const mainContent = catalogContent.querySelector('.catalog-item__main-content');
@@ -24,7 +38,7 @@ $(document).ready(function () {
         links.forEach(link => {
             link.addEventListener('click', (e) => {
                 e.preventDefault();
-
+    
                 if (mainContent.classList.contains('catalog-item__main-content_active')) {
                     mainContent.classList.remove('catalog-item__main-content_active');
                     listContent.classList.add('catalog-item__list-content_active');
@@ -32,7 +46,7 @@ $(document).ready(function () {
                 };
                 listContent.classList.remove('catalog-item__list-content_active');
                 mainContent.classList.add('catalog-item__main-content_active');
-
+    
             })
         })
     }
